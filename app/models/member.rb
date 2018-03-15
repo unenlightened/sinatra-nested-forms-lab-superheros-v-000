857442b3f1d8204a17c1c5e79d3f9cf36fd43d88
Chely Ho <adept_inept@yahoo.com>
2018-03-15 15:06:18 +0000

    automatically backed up by learn

---
+++ app/models/member.rb
@@ -0,0 +1,13 @@
+class Member
+  attr_accessor :name, :power, :bio
+  @@all = []
+
+  def self.all 
+    @@all
+  end
+
+  def initialize(:name, :power, :bio)
+    @name, @power, @bio = :name, :power, :bio
+  end
+
+end
